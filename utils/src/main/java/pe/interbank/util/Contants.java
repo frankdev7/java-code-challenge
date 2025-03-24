3bc3d3a775bd424c78a85d9beeca2b44f5fd0a61
frankdev7 <frank7rojas@gmail.com>
2025-03-24 12:01:20 -0500

    comments

--- utils/src/main/java/pe/interbank/util/Contants.java
+++ utils/src/main/java/pe/interbank/util/Contants.java
@@ -2,9 +2,11 @@ package pe.interbank.util;
 
 public class Contants {
 
+  // KAFKA
   public static final String GROUP_ID_INTERBANK = "pe.interbank";
   public static final String TOPIC_TRANSACTION_CREATED = "topic-transaction-created";
   public static final String TOPIC_TRANSACTION_VALIDATED = "topic-transaction-validated";
 
+  // AMOUNT
   public static final Double MAX_AMOUNT = 1000.0;
 }
